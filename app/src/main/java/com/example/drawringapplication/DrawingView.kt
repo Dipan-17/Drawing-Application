6d4cf1db6236893d7e4340deae14d4f0247d2cdf
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2024-01-13 17:01:11 +0530

    Image selector added

--- app/src/main/java/com/example/drawringapplication/DrawingView.kt
+++ app/src/main/java/com/example/drawringapplication/DrawingView.kt
@@ -107,6 +107,13 @@ class DrawingView(context:Context,attrs:AttributeSet): View(context,attrs) {
         mDrawPaint!!.strokeWidth=mBrushSize
     }
 
+    //method to set color from input from pallet clicking
+    fun setColor(newColor: String){
+        color=Color.parseColor(newColor)
+        mDrawPaint!!.color=color
+    }
+
+
     internal inner class CustomPath(var color:Int,
                                     var brushThickness:Float): Path() {
 
--- app/src/main/java/com/example/drawringapplication/MainActivity.kt
+++ app/src/main/java/com/example/drawringapplication/MainActivity.kt
@@ -3,18 +3,32 @@ package com.example.drawringapplication
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView:DrawingView?=null
+    private var mImageButtonCurrentPaint:ImageButton?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawingView=findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(10.toFloat())
+
+        val linearLayoutPaintColors=findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        //set default color as the color 0 -> skin color
+        mImageButtonCurrentPaint=linearLayoutPaintColors[0] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         val ib_brush: ImageButton =findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
@@ -48,5 +62,26 @@ class MainActivity : AppCompatActivity() {
 
         //show the dialog when this method is called
         brushDialog.show()
+    }
+
+    //method to set the color when clicked on the pallet
+    fun paintClicked(view: View){
+        //change only if a different color is selected
+        if(view!== mImageButtonCurrentPaint){
+            val imageButton=view as ImageButton
+            val colorTag=imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+
+            //update the background of the buttons
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+
+            //update the button
+            mImageButtonCurrentPaint=view
+        }
     }
 }
